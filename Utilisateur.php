940e7c4623cdd153c0357a2b97020f9d2ca1c9a5
Jensone <hello@jensone.com>
2025-04-10 11:07:45 +0200

    first commit

---
+++ Utilisateur.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ * Classe représentant un utilisateur
+ */
+
+class Utilisateur
+{
+    public $nom;
+    public $email;
+    public $identifiant;
+    public $dateInscription;
+
+    
+
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of identifiant
+     */ 
+    public function getIdentifiant()
+    {
+        return $this->identifiant;
+    }
+
+    /**
+     * Set the value of identifiant
+     *
+     * @return  self
+     */ 
+    public function setIdentifiant($identifiant)
+    {
+        $this->identifiant = $identifiant;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dateInscription
+     */ 
+    public function getDateInscription()
+    {
+        return $this->dateInscription;
+    }
+
+    /**
+     * Set the value of dateInscription
+     *
+     * @return  self
+     */ 
+    public function setDateInscription($dateInscription)
+    {
+        $this->dateInscription = $dateInscription;
+
+        return $this;
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'Utilisateur.php';
+
+$tom = new Utilisateur();
+$martin = new Utilisateur();
+
+var_dump($tom, $martin);
